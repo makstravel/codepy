25889a7e00b12df03f2adb97e64af87ec5a1eb3b
Maks_Travel <maks.traveler@gmail.com>
2022-12-22 19:58:01 +0300

    check

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # This is study Python.
+def get_lists(x):
+    v_min = min(x)
+    v_max = max(x)
+    v_sum = sum(x)
+    print(f'Min = {v_min}, max = {v_max}, sum = {v_sum}')
 
+get_lists(list(map(int, input().split())))
 
 
 
