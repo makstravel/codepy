7e96f918b6a47175b007041db8e48a502fce66b3
Maks_Travel <maks.traveler@gmail.com>
2022-12-22 21:45:10 +0300

    check

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 # This is study Python.
-def get_lists(x):
-    v_min = min(x)
-    v_max = max(x)
-    v_sum = sum(x)
-    print(f'Min = {v_min}, max = {v_max}, sum = {v_sum}')
-
-get_lists(list(map(int, input().split())))
-
-
+def info_kwargs(**kwargs):
+    return(sorted(kwargs))
 
