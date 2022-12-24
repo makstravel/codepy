c4c9df298148f5cb2694f6693f254f9ff8d450ba
Maks_Travel <maks.traveler@gmail.com>
2022-12-24 20:29:41 +0300

    check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # This is study Python.
-s1= input()
-s2= input()
-
-if s1 == s2[::-1]:
-     print('YES')
-else:
-     print('NO')
+subject_marks = [('English', 88), ('Science', 90), ('Maths', 97), ('Physics', 93),('History', 82)]
+s= sorted(subject_marks, key=lambda x: x[1], reverse=True)
+for i in s:
+    print(*i)
