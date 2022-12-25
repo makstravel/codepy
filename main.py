815cd24a6a5f46226593505ccee2edcedf817671
Maks_Travel <maks.traveler@gmail.com>
2022-12-25 21:55:27 +0300

    check

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-import turtle
-
-t = turtle.Turtle()
-t.screen.bgcolor('black')
-t.pensize(2)
-t.color('green')
-t.left(90)
-t.backward(100)
-t.speed(200)
-t.shape('turtle')
-
-
-def tree(i):
-    if i < 10:
-        return
-
-    else:
-        t.forward(i)
-        t.color('orange')
-        t.circle(2)
-        t.color('brown')
-        t.left(30)
-        tree(3 * i / 4)
-        t.right(60)
-        tree(3 * i / 4)
-        t.left(30)
-        t.backward(i)
-
-
-tree(100)
-turtle.done()
+people = [
+    ['Amy Smith', '694.322.8133x22426'],
+    ['Brian Shaw', '593.662.5217x338'],
+    ['Christian Sharp', '118.197.8810'],
+    ['Sean Schmidt', '9722527521'],
+    ['Thomas Long', '163.814.9938'],
+    ['Joshua Willis', '+1-978-530-6971x601'],
+    ['Ann Hoffman', '434.104.4302'],
+    ['John Leonard', '(956)182-8435'],
+    ['Daniel Ross', '870-365-8303x416'],
+    ['Jacqueline Moon', '+1-757-865-4488x652'],
+    ['Gregory Baker', '705-576-1122'],
+    ['Michael Spencer', '(922)816-0599x7007'],
+    ['Austin Vazquez', '399-813-8599'],
+    ['Chad Delgado', '979.908.8506x886'],
+    ['Jonathan Gilbert', '9577853541']
+]
+# people=dict(people)
+phone_book= {i[1]:i[0] for i in people}
+print(phone_book)
