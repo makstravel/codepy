57b5b20c3903166241ada6f539f32b843d5bd886
Maks_Travel <maks.traveler@gmail.com>
2022-12-26 20:51:54 +0300

    check

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def create_dict():
     count = 0
     d = {}
-    def cr_dcttwo(**kwargs):
+    def cr_dcttwo(s):
         nonlocal count, d
+        count +=1
+        d[count] = s
+        return d
+    return cr_dcttwo
 
