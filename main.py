0b8ca5973c0d1c03418a53cef39fdad0e0b796f3
Maks_Travel <maks.traveler@gmail.com>
2022-12-26 21:45:19 +0300

    check

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def create_dict():
-    count = 0
-    d = {}
-    def cr_dcttwo(s):
-        nonlocal count, d
-        count +=1
-        d[count] = s
-        return d
-    return cr_dcttwo
-
+import calendar
+c=calendar.TextCalendar()
+print(c.formatyear(2023))
