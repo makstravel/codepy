647abed42d209707a1e69acdcd8301edc460a144
Maks_Travel <maks.traveler@gmail.com>
2022-12-26 18:13:31 +0300

    check

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-def calculate(x:float, y:float, operation:str='a') -> None:
-    '''калькулятор'''
-    def addition():
-        print(float(x+y))
+def get_name_and_decades(name, age):
+    print(f"My name is {name} and I'm {age / 10:.5f} decades old.")
 
-    def subtraction():
-        print(float(x-y))
 
-    def division():
-        if y == 0:
-            print('На ноль делить нельзя!')
-        else:
-            print(float(x/y))
-
-    def multiplication():
-        print(float(x*y))
-
-    if operation== 'a':
-         addition()
-    elif operation == 's':
-         subtraction()
-    elif operation == 'd':
-         division()
-    elif operation == 'm':
-         multiplication()
-    else:
-        print('Ошибка. Данной операции не существует.')
-
-calculate(10,4)
+get_name_and_decades("Leo", 31)
