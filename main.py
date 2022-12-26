546edae8146ada80d977623734082a8e3b8fcfb2
Maks_Travel <maks.traveler@gmail.com>
2022-12-26 20:24:16 +0300

    check

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-def multiply(val):
-    def multiply_two(x):
-        print(f'Умножение {x} на {x} = {val*x}')
-    return multiply_two
-f_2 = multiply(2)
-f_2(5)
-f_2(15)
-f_3 = multiply(3)
-f_3(5)
-f_3(15)
-f_3(120)
-f_2(10024)
+def create_dict(**kwargs):
